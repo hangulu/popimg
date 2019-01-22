0604bf942e9f944aab58438df313b543d9379fe5
Hakeem Angulu <hakeem.angulu@gmail.com>
2019-01-22 07:28:03 -0500

    fix dependencies

---
+++ backend/popmemes/auth.py
@@ -0,0 +1,9 @@
+"""
+This module stores the keys for OAuth.
+"""
+
+# OAuthHandler parameters
+consumer_key = 'oLRMSWzLPNngsaVU1lrtnIanA'
+consumer_secret = '1Cr3MHmYiAVOh2cyGT9llqRAEsbgWfPbXLhenz54aTal3CFSkZ'
+access_token = '1436536897-igSiDebsFTrINAdlQ7ACZOxCR7PIHYUa8uZF0p0'
+access_secret = 'YlKllsRZukgCeQw8zVmfRuaZT8mC1E7N7MqWSzMY6gSIr'
